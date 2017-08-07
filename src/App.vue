893abc3b6c605bf2b794bbca0cf4278990fe5fb7
andreaxiang <xiangxue89@163.com>
2017-08-07 21:13:27 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      text: 'hello world'
+    }
+  }
+}
+</script>
+
+<style>
+  p {
+    color: red;
+    font-size: 40px;
+  }
+</style>
