5c40341f46d0dd8f9dc700e164a3bf555b81291b
andreaxiang <xiangxue89@163.com>
2017-08-07 22:08:07 +0800

    添加ResumeEditor左侧导航

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,18 +1,77 @@
 <template>
    <div id="resumeEditor">
-     I am resumeEditor
+     <nav>
+      <ol>
+        <li class="active"> xxx1 </li>
+        <li> xxx2 </li>
+        <li> xxx3 </li>
+        <li> xxx4 </li>
+        <li> xxx5 </li>
+        <li> xxx6 </li>
+        <li> xxx7 </li>
+      </ol>
+     </nav>
+      <ol class="panels">
+        <li>1111</li>
+        <li>2222</li>
+        <li>3333</li>
+        <li>4444</li>
+        <li>5555</li>
+        <li>6666</li>
+        <li>7777</li>
+      </ol>
    </div>
  </template>
  
  <script>
  
  export default {
-   name: 'ResumeEditor'
+   name: 'ResumeEditor',
+   data(){
+    return {
+
+    }
+   }
  }
  </script>
  
- <style>
- #resumeEditor {
-  color: red;
- }
+ <style lang="scss" scoped>
+  #resumeEditor {
+    background:#fff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+    display: flex;
+    flex-direction: row;
+    overflow: auto;
+    > nav {
+      width: 80px;
+      background: #0d331f;
+      color: #fff;
+      > ol {
+        > li {
+          height: 48px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding:2em 0;
+          &.active {
+            border-left: 4px solid #ff5b3b;
+            background: #fff;
+            color: #000;
+          }
+        }
+      }
+    }
+    > .panels {
+      flex-grow: 1;
+      > li {
+        padding: 24px;
+        > h2.item-title {
+          margin-bottom: .5em;
+        }
+      }
+    }
+  }
+  ol {
+    list-style: none;
+  }
  </style>
