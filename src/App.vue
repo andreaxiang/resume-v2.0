53e4613eba18564a96810a21ddc7961aa5c9cf1e
andreaxiang <xiangxue89@163.com>
2017-08-07 21:51:32 +0800

    使用scss，添加组件们的界面样式

--- src/App.vue
+++ src/App.vue
@@ -29,30 +29,42 @@ export default {
 }
 </script>
 
-<style>
-.page{
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-.page>main{
-  flex-grow: 1;
-}
+<style lang="scss">
+ .page {
+   height: 100vh;
+   display: flex;
+   flex-direction: column;
+   background: #f0efe9;
+   >main {
+     flex-grow: 1;
+   }
+   >main {
+     min-width: 1024px;
+     max-width: 1440px;
+     margin: 16px 0 0 0;
+     display: flex;
+     justify-content: space-between;
+     width: 100%;
+     align-self: center;
+   }
+ }
 
-.page>main{
-  min-width: 1024px;
-  max-width: 1440px;
-  margin: 0;
-  display: flex;
-  justify-content: space-around;
-}
+ #resumeEditor {
+   min-width: 35%;
+   background: #50e3a4;
+ }
 
-#resumeEditor{
-  width: 35%;
-  background: #444;
-}
-#resumePreview{
-  width: 61.66667%;
-  background: #777;
-}
+ #resumePreview {
+   flex-grow: 1;
+   margin-left: 16px;
+   background: #fff;
+ }
+
+ svg.icon {
+   height: 1em;
+   width: 1em;
+   fill: currentColor;
+   vertical-align: -0.1em;
+   font-size: 16px;
+ }
 </style>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,9 @@
 <template>
    <div id="topbar">
-     I am Topbar
+       <div class="logo">
+         <img src="https://ooo.0o0.ooo/2017/08/04/59841e858990d.png"/>
+         <span>在线简历</span>
+       </div>
    </div>
  </template>
  
@@ -11,10 +14,19 @@
  }
  </script>
  
- <style>
- #topbar {
-   background:#ffffff;
-   box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-   height:64px;
- }
+ <style lang="scss" scoped>
+  #topbar {
+    background:#fff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+    >.logo {
+      font-size:22px;
+      color:#0d331f;
+      >img {
+        width:50px;
+        height:45px;
+        margin-left: .2em;
+        vertical-align: middle;
+      }
+    }
+  }
  </style>
